be4a39541a52b9719462708bb43e7c29a1ec845d
Ryan Boldi <ryan.boldi123@gmail.com>
2023-01-14 20:17:07 -0500

    Update read_programs.clj

--- src/analysis/read_programs.clj
+++ src/analysis/read_programs.clj
@@ -172,6 +172,14 @@
        set
        count))
 
+(defn unique-behaviors
+  "number of unique behaviors in a pop (based on error)"
+  [pop]
+  (->> pop
+       (map #(:errors %))
+       set
+       count))
+
 (defn min-aggregate-error
   "best aggregate score of a population"
   [pop]
@@ -218,7 +226,8 @@
       (let [full-info-ds (sample-informed-ds (:x arguments) individuals data r 1)
             info-ds (sample-informed-ds (:x arguments) individuals data r 0.01)
             rand-ds (sample-random-ds (:x arguments) data r)
-            coverage {:full (coverage-ds full-info-ds) :info (coverage-ds info-ds) :rand (coverage-ds rand-ds)}
+            lex-cases data
+            coverage {:lex (coverage-ds lex-cases) :full (coverage-ds full-info-ds) :info (coverage-ds info-ds) :rand (coverage-ds rand-ds)}
             test-case-coverage-before (test-case-coverage individuals)
             ; for full, ids, rand. 
             ; A list of 10 populations that are selected with 10 successive downsamples of each kind
@@ -226,16 +235,22 @@
             full-selections (map #(ds-eval->full-eval individuals (select-population-with-ds individuals %)) full-info-ds)
             ids-selections (map #(ds-eval->full-eval individuals (select-population-with-ds individuals %)) info-ds)
             rand-selections (map #(ds-eval->full-eval individuals (select-population-with-ds individuals %)) rand-ds)
+            lex-selections (select-population-with-ds individuals lex-cases)
             ;test case coverage
             full-test-coverage (map #(test-case-coverage %) full-selections)
             ids-test-coverage (map #(test-case-coverage %) ids-selections)
             rand-test-coverage (map #(test-case-coverage %) rand-selections)
-            test-coverages {:before test-case-coverage-before :full full-test-coverage :ids ids-test-coverage :rand rand-test-coverage}
+            lex-test-coverage (test-case-coverage lex-selections)
+            test-coverages {:before test-case-coverage-before :full full-test-coverage :ids ids-test-coverage :rand rand-test-coverage :lex lex-test-coverage}
             ; phenotypic and genotypic entropy before
             ph-ent-before (phenotypic-entropy individuals)
             be-ent-before (behavioral-entropy individuals)
             id-ent-before (id-entropy individuals)
             ; after selection
+            ph-ent-lex (phenotypic-entropy lex-selections)
+            be-ent-lex (behavioral-entropy lex-selections)
+            id-ent-lex (id-entropy lex-selections)
+            ; ds
             full-ph-ent (map #(phenotypic-entropy %) full-selections)
             full-be-ent (map #(behavioral-entropy %) full-selections)
             full-id-ent (map #(id-entropy %) full-selections)
@@ -245,27 +260,37 @@
             rand-ph-ent (map #(phenotypic-entropy %) rand-selections)
             rand-be-ent (map #(behavioral-entropy %) rand-selections)
             rand-id-ent (map #(id-entropy %) rand-selections)
-            be-entropies {:before be-ent-before :full full-be-ent :ids ids-be-ent :rand rand-be-ent}
-            ph-entropies {:before ph-ent-before :full full-ph-ent :ids ids-ph-ent :rand rand-ph-ent}
-            id-entropies {:before id-ent-before :full full-id-ent :ids ids-id-ent :rand rand-id-ent}
+            be-entropies {:before be-ent-before :full full-be-ent :ids ids-be-ent :rand rand-be-ent :lex be-ent-lex}
+            ph-entropies {:before ph-ent-before :full full-ph-ent :ids ids-ph-ent :rand rand-ph-ent :lex ph-ent-lex}
+            id-entropies {:before id-ent-before :full full-id-ent :ids ids-id-ent :rand rand-id-ent :lex id-ent-lex}
             ;unique individuals selected
             before-unq-i (unique-individuals individuals)
+            lex-unq-i (unique-individuals lex-selections)
             full-unq-i (map #(unique-individuals %) full-selections)
             ids-unq-i (map #(unique-individuals %) ids-selections)
             rand-unq-i (map #(unique-individuals %) rand-selections)
             ;unique phenotypes
             before-unq-p (unique-phenotypes individuals)
+            lex-unq-p (unique-phenotypes lex-selections)
             full-unq-p (map #(unique-phenotypes %) full-selections)
             ids-unq-p (map #(unique-phenotypes %) ids-selections)
             rand-unq-p (map #(unique-phenotypes %) rand-selections)
-            unq-i {:before before-unq-i :full full-unq-i :ids ids-unq-i :rand rand-unq-i} 
-            unq-p {:before before-unq-p :full full-unq-p :ids ids-unq-p :rand rand-unq-p} 
+            ;unique behaviors
+            before-unq-b (unique-behaviors individuals)
+            lex-unq-b (unique-behaviors lex-selections)
+            full-unq-b (map #(unique-behaviors %) full-selections)
+            ids-unq-b (map #(unique-behaviors %) ids-selections)
+            rand-unq-b (map #(unique-behaviors %) rand-selections)
+            unq-i {:before before-unq-i :full full-unq-i :ids ids-unq-i :rand rand-unq-i :lex lex-unq-i} 
+            unq-p {:before before-unq-p :full full-unq-p :ids ids-unq-p :rand rand-unq-p :lex lex-unq-p} 
+            unq-b {:before before-unq-b :full full-unq-b :ids ids-unq-b :rand rand-unq-b :lex lex-unq-b} 
             ;minimum agg err
             before-min-err (min-aggregate-error individuals)
+            lex-min-error (min-aggregate-error lex-selections)
             full-min-err (map #(min-aggregate-error %) full-selections)
             ids-min-err (map #(min-aggregate-error %) ids-selections)
             rand-min-err (map #(min-aggregate-error %) rand-selections)
-            min-err {:before before-min-err :full full-min-err :ids ids-min-err :rand rand-min-err}]
+            min-err {:before before-min-err :full full-min-err :ids ids-min-err :rand rand-min-err :lex lex-min-error}]
         {:r r
          :coverage coverage
          :test-coverages test-coverages
@@ -274,6 +299,7 @@
          :id-entropies id-entropies
          :unique-individuals unq-i
          :unique-phenotypes unq-p
+         :unique-behaviors unq-b
          :min-error min-err})))))
 
 
