5c2db48ab8de8077eec8a44ab41ce50016de5326
Ryan Boldi <ryan.boldi123@gmail.com>
2023-02-17 15:13:35 -0500

    Update read_programs.clj

--- src/analysis/read_programs.clj
+++ src/analysis/read_programs.clj
@@ -236,7 +236,7 @@
                     :n 1000}
                    (apply hash-map (map #(if (string? %) (read-string %) %) args)))
         gen (:gen arguments); potentially should use gen 0, gen middle, and last gen
-        file (str "./run-data/par-" (:prefix arguments) "-lexicase-case-maxmin-0.05-0.01-100-" gen ".edn")
+        file (str "./run-data/par-" (:prefix arguments) "-lexicase-full-" gen ".edn")
         data (initialise-cases (get-train-data-from-prefix (:prefix arguments)))
         err-func (get-error-fn-from-prefix (str (:prefix arguments)))
         individuals (h/reindex-pop (evaluate-push-population {:step-limit 2000} data file err-func))]
